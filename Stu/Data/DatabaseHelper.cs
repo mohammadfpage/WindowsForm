6dd5df25f282e3b364f97556f929efc1eab8f2e7
mohammadfpage <fallahi.mq@gmail.com>
2025-05-01 14:30:05 -0700

    push in OK

--- Stu/Data/DatabaseHelper.cs
+++ Stu/Data/DatabaseHelper.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Data.SqlClient;
 using System;
+using System.Diagnostics;
+using System.IO;
 
 public static class DatabaseHelper
 {
@@ -7,10 +9,31 @@ public static class DatabaseHelper
 
     static DatabaseHelper()
     {
-        string dbPath = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Database", "School.mdf");
+        // مسیر نصب (فایل اصلی دیتابیس در کنار exe)
+        string sourcePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Database", "School.mdf");
 
+        // مسیر مقصد امن برای دیتابیس (داخل LocalAppData)
+        string localAppData = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+        string appFolder = Path.Combine(localAppData, "SchoolApp", "Database");
+        string targetPath = Path.Combine(appFolder, "School.mdf");
+
+        // اگر دیتابیس هنوز به LocalAppData کپی نشده
+        if (!File.Exists(targetPath))
+        {
+            Directory.CreateDirectory(appFolder);
+            if (File.Exists(sourcePath))
+            {
+                File.Copy(sourcePath, targetPath);
+            }
+            else
+            {
+                throw new FileNotFoundException("فایل دیتابیس در مسیر نصب پیدا نشد.", sourcePath);
+            }
+        }
+
+        // رشته اتصال به دیتابیس از مسیر LocalAppData
         _connectionString = $@"Data Source=(LocalDB)\ProjectsV16;
-                            AttachDbFilename={dbPath};
+                            AttachDbFilename={targetPath};
                             Integrated Security=True;
                             Connect Timeout=30;";
     }
